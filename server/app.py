454084bc530728f60dc637d9d18f6af6003610f8
zfdupont <zachary.dupont@stonybrook.edu>
2024-07-15 15:50:38 -0400

    naming and cors pt 2

--- server/app.py
+++ server/app.py
@@ -1,14 +1,22 @@
-from flask import Flask
+from flask import Flask, request as req
 from flask_cors import CORS, cross_origin
 import pandas as pd
+import logging
+
+logging.getLogger('flask_cors').level = logging.DEBUG
 
 app = Flask(__name__)
 cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['CORS_ALWAYS_SEND'] = True
+app.config['CORS_SEND_WILDCARD'] = True
+app.config['CORS_SUPPORTS_CREDENTIALS'] = False
+
+
+
+# app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/api/players')
-@cross_origin('http:localhost:3000')
-def hello_world():
-    df = pd.read_csv('../wnbabpm.csv', index_col=0).rename({ 'Off. Role': 'offRole'})
-    # print(df)
+def list_players():
+    df = pd.read_csv('../wnbabpm.csv', index_col=0).sort_values(req.args.get('sort'), ascending=req.args.get('order') == 'ASC').rename(columns={ 'Off. Role': 'offRole', 'Pos': 'position'}).round(3)
+    df.columns = map(str.lower, df.columns)
     return df.to_dict(orient='records')
