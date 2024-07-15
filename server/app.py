9c7ba9d9f5eeb81250bda099aa0cd76557518d32
zfdupont <zachary.dupont@stonybrook.edu>
2024-07-15 10:09:11 -0400

    cors

--- server/app.py
+++ server/app.py
@@ -7,7 +7,7 @@ cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/api/players')
-@cross_origin()
+@cross_origin('http:localhost:3000')
 def hello_world():
     df = pd.read_csv('../wnbabpm.csv', index_col=0).rename({ 'Off. Role': 'offRole'})
     # print(df)
