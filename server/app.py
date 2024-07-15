84987f5f1386b526033e3fe3023d4a121ce5cd66
zfdupont <zachary.dupont@stonybrook.edu>
2024-07-15 10:00:57 -0400

    cors + naming

--- server/app.py
+++ server/app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 import pandas as pd
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route('/')
+@app.route('/api/players')
+@cross_origin()
 def hello_world():
-    df = pd.read_csv('../wnbabpm.csv', index_col=0)
+    df = pd.read_csv('../wnbabpm.csv', index_col=0).rename({ 'Off. Role': 'offRole'})
     # print(df)
     return df.to_dict(orient='records')
